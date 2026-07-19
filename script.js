867e8473de8814242fa0d1daa58dc14e962f30a4
shreekesharbhavaniseedtech832 <shreekesharbhavaniseedtech832@gmail.com>
2026-07-19 08:43:53 -0700

    Refactor slider and search functionality comments

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Slider Auto
+// Slider
 let slides = document.querySelectorAll(".slide");
 let index = 0;
 
@@ -8,8 +8,7 @@ setInterval(()=>{
   slides[index].classList.add("active");
 },3000);
 
-
-// Search Filter
+// Search
 document.getElementById("search").addEventListener("keyup", function(){
   let value = this.value.toLowerCase();
   let cards = document.querySelectorAll(".card");
@@ -20,10 +19,9 @@ document.getElementById("search").addEventListener("keyup", function(){
   });
 });
 
-
 // WhatsApp Order
 function order(product){
-  let number = "919999999999";
+  let number = "917096438261";
   let msg = "I want to buy " + product;
   window.open(`https://wa.me/${number}?text=${msg}`);
 }
